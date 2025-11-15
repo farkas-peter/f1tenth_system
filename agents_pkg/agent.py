73c3b08fc511ca541b8919c99c71269629447da2
farkas-peter <peterfarkas@edu.bme.hu>
2025-11-15 22:02:40 +0100

    chg: refactor & audio transcription

--- agents_pkg/agent.py
+++ agents_pkg/agent.py
@@ -1,19 +1,14 @@
-import sys
-from google import genai
-from google.genai import types
-import cv2
+import os
 import json
-from dotenv import load_dotenv
-from PIL import Image
+
+import cv2
 import numpy as np
 import pyrealsense2 as rs
-import sounddevice as sd
-from scipy.io.wavfile import write
-import time
-from tqdm import tqdm
+from PIL import Image
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
-# Init
-print(sys.version)
 load_dotenv()
 
 class Agent:
@@ -24,24 +19,27 @@ class Agent:
 
         # Camera
         self.cam_pipeline = rs.pipeline()
-        self.cam_config = rs.config()
-        #self.cam_config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        self.cam_config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-        self.cam_pipeline.start(self.cam_config)
+        cam_config = rs.config()
+        cam_config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        self.cam_pipeline.start(cam_config)
     
-    def detect_object(self, obj_to_detect, image):
+    def detect_object(self, obj_to_detect, image, visualize=False):
+        # Detect object in image using Gemini
         prompt = (
             f"Detect the {obj_to_detect} in the image. "
             f"The box_2d should be [ymin, xmin, ymax, xmax] normalized to 0-1000."
         )
 
-        response = self.client.models.generate_content(model="gemini-2.5-flash",
+        config = types.GenerateContentConfig(response_mime_type="application/json")
+        response = self.client.models.generate_content(model=self.gemini_model,
                                                        contents=[image, prompt],
-                                                       config=self.config
+                                                       config=config
                                                        )
-        
+
+        # Convert normalized bb to absolute pixel values
         width, height = image.size
         bounding_boxes = json.loads(response.text)
+        bounding_boxes = bounding_boxes if isinstance(bounding_boxes, list) else [bounding_boxes]
 
         converted_bounding_boxes = []
         for bounding_box in bounding_boxes:
@@ -54,10 +52,14 @@ class Agent:
         print("Image size: ", width, height)
         print("Bounding boxes:", converted_bounding_boxes)
 
-        self.visualize_detections(image, converted_bounding_boxes)
+        if visualize:
+            self.visualize_detections(image, converted_bounding_boxes)
 
-    def visualize_detections(self, image, converted_bounding_boxes):
-        # Visualize the image with the bb
+        return converted_bounding_boxes
+
+    @staticmethod
+    def visualize_detections(image, converted_bounding_boxes):
+        # Visualize the image with the detection bounding boxes
         cv_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
         for box in converted_bounding_boxes:
             cv2.rectangle(cv_image, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
@@ -65,11 +67,35 @@ class Agent:
         cv2.waitKey(0)
 
     def capture_image(self):
+        # Captures frame from RealSense camera
         frames = self.cam_pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
         color_image = np.asanyarray(color_frame.get_data())
 
-        return color_image
+        return Image.fromarray(color_image)
+
+    def get_obj_desc_from_audio(self, audio_filepath):
+        # Get object description from an audio file using Gemini
+        prompt = f"""
+        You are an assistant that interprets spoken commands for visual search.
+        Task: Process the provided audio file and extract the main object with all of its properties or spatial information.
+        Ignore all filler words like 'go to', 'please', 'look for', etc.
+        Return only a short clean phrase describing the search target.
+        """
+
+        with open(audio_filepath, 'rb') as f:
+            audio_bytes = f.read()
+
+        response = self.client.models.generate_content(
+            model=self.gemini_model,
+            contents=[prompt,
+                      types.Part.from_bytes(data=audio_bytes, mime_type='audio/wav')
+                      ]
+        )
+
+        print("Object description from audio:", response.text)
+
+        return response.text
 
 
 if __name__ == "__main__":
@@ -80,4 +106,5 @@ if __name__ == "__main__":
     image = agent.capture_image()
     image = Image.fromarray(image)
 
-    agent.detect_object("controller", image)
+    #obj = agent.get_obj_desc_from_audio("temp_audio.wav")
+    bbs = agent.detect_object("object above the pizza", img, visualize=True)
