179bb878a839e87984888a1e6082406e33494d40
farkas-peter <peterfarkas@edu.bme.hu>
2025-11-16 21:48:10 +0100

    chg: camera stuff

--- agents_pkg/speech_to_obj_agent.py
+++ agents_pkg/speech_to_obj_agent.py
@@ -1,24 +1,48 @@
 import cv2
-import requests
+import pyrealsense2 as rs
+import numpy as np
 
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 
-class SpeechToObjAgent(Node):
+class ObjTrackerNode(Node):
     def __init__(self):
-        super().__init__('speech_to_obj_agent')
+        super().__init__('obj_tracker_node')
 
-        self.last_received_value = None
+        # Camera variables
+        self.pipeline = rs.pipeline()
+        config = rs.config()
+        config.enable_stream(rs.stream.color, 848, 480, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 30)
+        self.pipeline.start(config)
+        self.align = rs.align(rs.stream.color)
 
+        # ROS2 variables
+        self.subscription = self.create_subscription(String, '/my_topic', self.listener_callback, 10)
         self.timer = self.create_timer(0.033, self.camera_callback)  # 30 FPS
 
-        self.subscription = self.create_subscription(String, '/my_topic', self.listener_callback, 10)
-
-        self.get_logger().info("SpeechToObjAgent node started.")
+        self.get_logger().info("ObjTrackerNode node started.")
 
     def camera_callback(self):
-        pass
+        # Wait for frame and align the color and depth frames
+        frames = self.pipeline.wait_for_frames()
+        aligned_frames = self.align.process(frames)
+
+        depth_frame = aligned_frames.get_depth_frame()
+        color_frame = aligned_frames.get_color_frame()
+
+        if not depth_frame or not color_frame:
+            return
+
+        # Visualize
+        color_np_image = np.asanyarray(color_frame.get_data())
+        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+        cv2.imshow('RealSense', color_np_image)
+        cv2.waitKey(1)
+
+        # todo: When the node started, let's save an image for obj detection
+
 
     def tracking(self, frame, bbox):
         tracker = cv2.legacy.TrackerCSRT_create()
@@ -35,19 +59,18 @@ class SpeechToObjAgent(Node):
                 # Tracker should be initialized again
                 pass
 
-
     def listener_callback(self, msg):
-        self.last_received_value = msg.data  # store message
-        self.get_logger().info(f"Received: {self.last_received_value}")
+        data = msg.data  # store message
+        self.get_logger().info(f"Received: {data}")
 
     def shutdown(self):
         self.pipeline.stop()
-        self.get_logger().info("SpeechToObjAgent node stopped.")
+        self.get_logger().info("ObjTrackerNode node stopped.")
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = SpeechToObjAgent()
+    node = ObjTrackerNode()
 
     try:
         rclpy.spin(node)
