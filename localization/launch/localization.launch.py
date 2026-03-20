9e6f8abc41b2bdbc0565d1b4d69fe66b01d618e2
farkas-peter <peterfarkas@edu.bme.hu>
2026-03-20 12:57:44 +0000

    robot_localization package init

---
+++ localization/launch/localization.launch.py
@@ -0,0 +1,57 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # Paths
+    localization_dir = get_package_share_directory('localization')
+    ekf_config = os.path.join(localization_dir, 'config', 'ekf.yaml')
+    navsat_config = os.path.join(localization_dir, 'config', 'navsat.yaml')
+
+    ublox_gps_dir = get_package_share_directory('ublox_gps')
+    ublox_gps_launch_file = os.path.join(ublox_gps_dir, 'launch', 'ublox_gps_node-launch.py')
+
+    ntrip_client_dir = get_package_share_directory('ntrip_client')
+    ntrip_client_launch_file = os.path.join(ntrip_client_dir, 'ntrip_client_launch.py')
+
+    # GPS and NTRIP IncludeLaunchDescriptions
+    ublox_gps_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(ublox_gps_launch_file)
+    )
+
+    ntrip_client_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(ntrip_client_launch_file)
+    )
+
+    # Robot Localization Nodes
+    ekf_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=[ekf_config]
+    )
+
+    navsat_transform_node = Node(
+        package='robot_localization',
+        executable='navsat_transform_node',
+        name='navsat_transform',
+        output='screen',
+        parameters=[navsat_config],
+        remappings=[
+            ('imu', '/imu'),
+            ('gps/fix', '/fix'),
+            ('odometry/gps', '/odometry/gps'),
+            ('odom', '/odometry/filtered')
+        ]
+    )
+
+    return LaunchDescription([
+        ublox_gps_launch,
+        ntrip_client_launch,
+        ekf_node,
+        navsat_transform_node
+    ])
