5914c337526cd638a48bf1d3fc7eb2797bb3b3b8
boroscsaba00 <csaba.boros@edu.bme.hu>
2026-04-27 14:22:07 +0000

    Robot_localization pkg removed from loc_launch

--- localization/launch/localization.launch.py
+++ localization/launch/localization.launch.py
@@ -6,11 +6,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    # Paths
-    localization_dir = get_package_share_directory('localization')
-    ekf_config = os.path.join(localization_dir, 'config', 'ekf.yaml')
-    navsat_config = os.path.join(localization_dir, 'config', 'navsat.yaml')
-
     ublox_gps_dir = get_package_share_directory('ublox_gps')
     ublox_gps_launch_file = os.path.join(ublox_gps_dir, 'launch', 'ublox_gps_node-launch.py')
 
@@ -50,36 +45,6 @@ def generate_launch_description():
         arguments=["0.33", "0", "0.55", "0", "0", "0", "base_link", "lidar_link"],
     )
 
-    # Robot Localization Nodes
-    ekf_node = Node(
-        package='robot_localization',
-        executable='ekf_node',
-        name='ekf_filter_node',
-        output='screen',
-        parameters=[ekf_config]
-    )
-
-    navsat_transform_node = Node(
-        package='robot_localization',
-        executable='navsat_transform_node',
-        name='navsat_transform',
-        output='screen',
-        parameters=[navsat_config],
-        remappings=[
-            ('imu', '/imu'),
-            ('gps/fix', '/fix'),
-            ('odometry/gps', '/odometry/gps'),
-            ('odom', '/odometry/filtered')
-        ]
-    )
-
-    realsense_imu_node = Node(
-        package='realsense',
-        executable='imu',
-        name='imu_node',
-        output='screen'
-    )
-
     coord_trans_node = Node(
         package='coord_trans',
         executable='coord_trans_node',
@@ -100,9 +65,6 @@ def generate_launch_description():
         tf_base_gps,
         tf_base_imu,
         tf_base_lidar,
-        ekf_node,
-        navsat_transform_node,
-        realsense_imu_node,
         coord_trans_node,
         localization_vis_node
     ])
